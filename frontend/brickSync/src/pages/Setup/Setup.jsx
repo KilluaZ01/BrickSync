9ec261a7204415f02d273dac63f8a461f6f5bb89
arik <raiarik97@gmail.com>
2024-08-01 22:59:20 +0545

    Setup updates

--- frontend/brickSync/src/pages/Setup/Setup.jsx
+++ frontend/brickSync/src/pages/Setup/Setup.jsx
@@ -5,20 +5,20 @@ import { HiUserGroup } from "react-icons/hi";
 
 const Setup = () => {
   return (
-    <div className="bg-[rgba(34,40,49,1)] flex flex-col justify-center items-center h-screen">
+    <div className="bg-[rgba(34,40,49,1)] flex flex-col justify-center items-center h-screen gap-[60px]">
       <div className="flex flex-col items-center">
         <img className="w-[200px]" src={assets.logo_2} alt="" />
         <h3 className="text-[16px] text-[#eee]">
           Keeping everything synchronized
         </h3>
       </div>
-      <div className="flex flex-row">
-        <div className="flex flex-col text-[#eee] text-[50px] font-semibold max-w-[250px] items-center text-center">
+      <div className="flex flex-row items-center gap-[60px]">
+        <div className="flex flex-col text-[#eee] text-[30px] font-semibold max-w-[190px] items-center text-center ">
           <HiUser className="w-[60px] h-auto text-[#eee]" />
           Setup a Business
         </div>
         <div className="inline-block h-[250px] min-h-[1em] w-[1px] self-stretch bg-[hsla(0,0%,93%,0.8)]"></div>
-        <div className="flex flex-col text-[#eee] text-[50px] font-semibold max-w-[250px] items-center text-center">
+        <div className="flex flex-col text-[#eee] text-[30px] font-semibold max-w-[190px] items-center text-center">
           <HiUserGroup className="w-[60px] h-auto text-[#eee]" />
           Join a Business
         </div>
